8675b90684136d15e1150fa0e2b2486bf39dcc40
Bokeher <bokeher47@gmail.com>
2026-08-16 20:07:52 +0200

    fix(timetable): capitalize first letter of subject name

--- app/src/main/java/com/example/planlekcji/ckziu_elektryk/client/timetable/lesson/Subject.java
+++ app/src/main/java/com/example/planlekcji/ckziu_elektryk/client/timetable/lesson/Subject.java
@@ -1,4 +1,9 @@
 package com.example.planlekcji.ckziu_elektryk.client.timetable.lesson;
 
 public record Subject(String name, String shortcut) {
+    public Subject {
+        if (name != null && !name.isEmpty()) {
+            name = Character.toUpperCase(name.charAt(0)) + name.substring(1);
+        }
+    }
 }
